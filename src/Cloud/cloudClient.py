3a4ba7ba0f496db1878872431d6f7eb7183a15cf
JoshR <joshrawlinson05@outlook.com>
2025-08-12 16:17:15 +0100

    Add error check and class defined cloud variables

--- src/Cloud/cloudClient.py
+++ src/Cloud/cloudClient.py
@@ -2,30 +2,46 @@ import requests
 from datetime import timezone, datetime
 
 class Cloud:
+    """
+    This class defines the cloud
 
-    def __init__(self, URL:str, timeout:int=1):
+    argv:
+        URL              (str)  The URL of targetted cloud service
+        timout           (int)  Number of seconds until failed connection to cloud causes timeout
+        return_variables (list) List of string variables that should want returning from cloud 
+    """
+
+    def __init__(self, URL:str, timeout:int=1, return_variables:list=[]):
         if not URL:
             raise TypeError("Provide a URL to Cloud Service")
         self.URL = URL
+        self.return_variables = return_variables
+        self.timeout = timeout
 
     def send_dataset(self, payload):
+        """
+        Sends user inputted JSON `payload` to object defined URL
+        Returns a dictionary of variables expected from cloud and time client recieved
+        """
         try:
-            response = requests.post(self.URL, json=payload)
+            response = requests.post(self.URL, json=payload, timeout=self.timeout)
             client_recieve_time = datetime.now(timezone.utc)
 
             if response.status_code == 200:
                 cloud_return = response.json()
-                returned_value = cloud_return.get('response_variable')
-                client_send_time = cloud_return.get('client_send_time')
-                feedback = {
-                    'response_variable': returned_value,
-                    'client_send_time': client_send_time,
-                    'client_recieve_time': client_recieve_time
-                }
-                return feedback
+                try:
+                    if list:
+                        feedback = {}
+                        for variable in self.return_variables:
+                            feedback[variable] = cloud_return.get(variable)
+                            feedback['client_recieve_time'] = client_recieve_time
+                        return feedback
+                    else:
+                        print("ERROR: No expected return from cloud")
+                except TypeError("ERROR: variable(s) expect from cloud do not exist"):
+                    return None
             else:
                 print(f"ERROR: Cloud Status Code {response.status_code} ")
         except requests.exceptions.RequestException as e:
             print(f"ERROR: Network Connection failed\n{e}")
             return None
-
